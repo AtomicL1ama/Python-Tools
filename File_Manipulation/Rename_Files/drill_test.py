f879ef72d2258c042240dced449966942cd9eea6
Garrett Moore <garrett.m.moore@gmail.com>
2023-01-10 23:34:43 -0500

    Create drill_test.py
    drill_test was made as a way of testing easier ways to use rename_files.py.  I found it simplest to detect for a ".", as Windows document structure won't allow for it in folder names.

---
+++ File_Manipulation/Rename_Files/drill_test.py
@@ -0,0 +1,28 @@
+import os, time
+
+def getPath():
+    return input('Provide a folder path to review it\'s contents: ')
+
+def printDir(path):
+    dirs = os.listdir(path)
+    for file in dirs:
+        print(file)
+        if (file.find(".")<0):
+            print(f"Opening \"{file}\"...")
+            printDir(f"{path}/{file}")
+
+def breakPath(path):
+    x = path.rindex("\\")+1
+    path_root = path[:x]
+    root_folder = path[x:]
+    return (path_root, root_folder)
+
+def begin():
+    target_path = getPath()
+    target_roots = breakPath(target_path)
+    print(f"Showing contents of {target_roots[-1]}...")
+    printDir(target_path)
+
+#~~~~~~~~~~~~~~~~~~~~ MAIN ~~~~~~~~~~~~~~~~~~~~
+
+begin()
